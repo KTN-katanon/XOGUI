e2eae23e4f0d25289644c7401fd6b465aa626890
KTN-katanon <66160196@go.buu.ac.th>
2024-08-02 18:31:22 +0700

    Create Project

---
+++ XOGui/src/main/java/com/katanon/xogui/XOGui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.katanon.xogui;
+
+/**
+ *
+ * @author nonku
+ */
+public class XOGui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
