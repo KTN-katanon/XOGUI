d1295b81b37becc1b08c0c73bb3a574974126d90
KTN-katanon <66160196@go.buu.ac.th>
2024-08-02 23:54:42 +0700

    Copy Board and Player Class
    Copy Board and Player Class from OOPXO project

---
+++ XOGui/src/main/java/com/katanon/xogui/Board.java
@@ -0,0 +1,142 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.katanon.xogui;
+
+/**
+ *
+ * @author nonku
+ */
+public class Board {
+
+    private char[][] board = {{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
+    private Player p1;
+    private Player p2;
+    private Player currentPlayer;
+
+    public Board(Player p1, Player p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+        currentPlayer = p1;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(char[][] board) {
+        this.board = board;
+    }
+
+    public Player getP1() {
+        return p1;
+    }
+
+    public void setP1(Player p1) {
+        this.p1 = p1;
+    }
+
+    public Player getP2() {
+        return p2;
+    }
+
+    public void setP2(Player p2) {
+        this.p2 = p2;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public boolean setRowCol(int row, int col) {
+        if (board[row][col] != '-') {
+            return false;
+        }
+        board[row][col] = currentPlayer.getSymbol();
+        return true;
+    }
+
+    public void switchTurn() {
+        if (currentPlayer == p1) {
+            currentPlayer = p2;
+        } else if (currentPlayer == p2) {
+            currentPlayer = p1;
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean checkWinner() {
+        // check row
+        if ((board[0][0] == currentPlayer.getSymbol() && board[0][1] == currentPlayer.getSymbol() && board[0][2] == currentPlayer.getSymbol())
+                || (board[1][0] == currentPlayer.getSymbol() && board[1][1] == currentPlayer.getSymbol() && board[1][2] == currentPlayer.getSymbol())
+                || (board[2][0] == currentPlayer.getSymbol() && board[2][1] == currentPlayer.getSymbol() && board[2][2] == currentPlayer.getSymbol())) {
+            return true;
+        } // check col
+        else if ((board[0][0] == currentPlayer.getSymbol() && board[1][0] == currentPlayer.getSymbol() && board[2][0] == currentPlayer.getSymbol())
+                || (board[0][1] == currentPlayer.getSymbol() && board[1][1] == currentPlayer.getSymbol() && board[2][1] == currentPlayer.getSymbol())
+                || (board[0][2] == currentPlayer.getSymbol() && board[1][2] == currentPlayer.getSymbol() && board[2][2] == currentPlayer.getSymbol())) {
+            return true;
+        } // check X
+        else if (board[0][0] == currentPlayer.getSymbol() && board[1][1] == currentPlayer.getSymbol() && board[2][2] == currentPlayer.getSymbol()
+                || board[0][2] == currentPlayer.getSymbol() && board[1][1] == currentPlayer.getSymbol() && board[2][0] == currentPlayer.getSymbol()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean results() {
+        if (checkWinner()) {
+            printBoard();
+            System.out.println(currentPlayer);
+            return true;
+        } else if (isBoardFull()) {
+            printBoard();
+            System.out.println("Draw!!!");
+            return true;
+        }
+        return false;
+    }
+
+    public void updateStat() {
+        if (checkWinner()) {
+            if (currentPlayer == p1) {
+                p1.incWinCount();
+                p2.incLoseCount();
+            } else if (currentPlayer == p1) {
+                p2.incWinCount();
+                p1.incLoseCount();
+            } else {
+                
+            }
+        }
+        if (isBoardFull()) {
+            p2.incDrawCount();
+            p1.incDrawCount();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" + "board=" + board + ", p1=" + p1 + ", p2=" + p2 + '}';
+    }
+}
---
+++ XOGui/src/main/java/com/katanon/xogui/Player.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.katanon.xogui;
+
+/**
+ *
+ * @author nonku
+ */
+public class Player {
+    private char symbol;
+    private int winCount;
+    private int drawCount;
+    private int loseCount;
+
+    public Player(char symbol) {
+        this.symbol = symbol;
+        this.winCount = 0;
+        this.drawCount = 0;
+        this.loseCount = 0;
+    }
+
+    public int getWinCount() {
+        return winCount;
+    }
+
+    public void incWinCount() {
+        this.winCount++;
+    }
+
+    public int getDrawCount() {
+        return drawCount;
+    }
+
+    public void incDrawCount() {
+        this.drawCount++;
+    }
+
+    public int getLoseCount() {
+        return loseCount;
+    }
+
+    public void incLoseCount() {
+        this.loseCount++;
+    }
+    
+    public char getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "symbol=" + symbol + ", winCount=" + winCount + ", drawCount=" + drawCount + ", loseCount=" + loseCount + '}';
+    }
+
+    
+    
+    
+}
